42d065e83782ed61dc995d1f3dc70ac30176cd60
David Gutierrez Mesa <david.gutierrez@liferay.com>
2022-03-23 16:38:34 +0100

    Update MyAmazingFragmentPaths.java

--- src/test/java/portalObjects/paths/MyAmazingFragmentPaths.java
+++ src/test/java/portalObjects/paths/MyAmazingFragmentPaths.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class MyAmazingFragmentPaths {
 
-    //TODO Step 2 Assertion
+    //TODO Step 3 Assertion
     public static final By FIRST_NAME_LOCATOR = null;
     public static final By EMAIL_LOCATOR = null;
 
